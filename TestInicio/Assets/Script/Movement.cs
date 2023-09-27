436360ca246de05b399c3edf9aa16be4a83eaabe
Tomatitofrito03 <146166766+Tomatitofrito03@users.noreply.github.com>
2023-09-27 10:19:57 +0200

    Trabajo_inicial_script_mov

--- TestInicio/Assets/Script/Movement.cs
+++ TestInicio/Assets/Script/Movement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate() // se llama sí o sí 100 veces por segundo
+    {
+
     }
 }
